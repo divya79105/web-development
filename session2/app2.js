ece8d1de776c169af05eb5f0f4b4d9df9e9fa0a8
DIVYA79105 <divyamanju79105@gmail.com>
2019-10-26 14:07:13 +0530

    uploaded

---
+++ session2/app2.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    alert();
+}
+function sayHi(){
+    //debugger
+    console.log(event.target.value)
+    document.getElementById("changed").innerHTML=event.target.value
+   // console.log(event.clientX+ ':'+event.clientY)
+}
