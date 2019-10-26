a58a47237c08d82f84b8c1d3aceabdb1f7e8ae9a
DIVYA79105 <divyamanju79105@gmail.com>
2019-10-26 09:31:27 +0530

    uploaded

---
+++ app1.js
@@ -0,0 +1,45 @@
+/*console.log("Iam new user");
+console.error("occured error");
+console.warn("Its a warning");
+//alert("Iam alert");
+var name='divya';
+console.log(name+" is my name")
+if (true){
+    console.log(name+" inside if")
+}
+console.log(name+"ouside if")
+
+
+var letvar = 'Iam outside'
+if (true){
+    varletvar='Iam Inside'
+    console.log(letvar)
+}
+console.log(letvar);
+function vtu()
+{
+    console.log('Iam inside vtu')
+    console.log(window)
+}
+vtu();
+var name='vtu'
+console.log(name)
+function sjcit(){
+    let name='sjcit'
+    console.log(name+" inside vtu");
+    console.log(window)
+}
+sjcit();
+//function sjcit()
+//{
+  //  alert("I am function")
+//}
+//sjcit();*/
+function myFunction(elem){
+    console.log(elem)
+    elem.style.color="red"
+}
+function sjcit(ele){
+    console.log(ele)
+    ele.style.changeto="sjcit"
+}
